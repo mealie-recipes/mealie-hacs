96f7e0990705cbca0efb2de5057b65e5a8b6bd48
hay-kot <hay-kot@pm.me>
2021-11-22 18:42:02 -0900

    init

---
+++ custom_components/mealie/binary_sensor.py
@@ -0,0 +1,33 @@
+"""Binary sensor platform for Mealie."""
+from homeassistant.components.binary_sensor import BinarySensorEntity
+
+from .const import BINARY_SENSOR
+from .const import BINARY_SENSOR_DEVICE_CLASS
+from .const import DEFAULT_NAME
+from .const import DOMAIN
+from .entity import MealieEntity
+
+
+async def async_setup_entry(hass, entry, async_add_devices):
+    """Setup binary_sensor platform."""
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    async_add_devices([MealieBinarySensor(coordinator, entry)])
+
+
+class MealieBinarySensor(MealieEntity, BinarySensorEntity):
+    """mealie binary_sensor class."""
+
+    @property
+    def name(self):
+        """Return the name of the binary_sensor."""
+        return f"{DEFAULT_NAME}_{BINARY_SENSOR}"
+
+    @property
+    def device_class(self):
+        """Return the class of this binary_sensor."""
+        return BINARY_SENSOR_DEVICE_CLASS
+
+    @property
+    def is_on(self):
+        """Return true if the binary_sensor is on."""
+        return self.coordinator.data.get("title", "") == "foo"
